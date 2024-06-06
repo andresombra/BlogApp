0b53ae26cc1e17f31b6983b9b2d8be25f923acab
andresombra <andresombra.net@gmail.com>
2024-06-06 12:25:13 -0300

    Implementado Client WebSocket no Frontend

--- BlogApp/obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
+++ BlogApp/obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+484ab41e43476cdae3aa75a1b56b1285597ce678")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+742e5dc52f3d210ca9149ea20218a32adbf6c8ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BlogApp/wwwroot/js/site.js
+++ BlogApp/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const socket = new WebSocket('ws://localhost:5000/ws');
+
+socket.onopen = function (event) {
+    console.log('WebSocket is open now.');
+};
+
+socket.onmessage = function (event) {
+    console.log('WebSocket message received:', event.data);
+    alert(event.data);
+};
+
+socket.onclose = function (event) {
+    console.log('WebSocket is closed now.');
+};
+
+socket.onerror = function (error) {
+    console.log('WebSocket error:', error);
+};
